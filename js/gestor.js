813b7b5bf89f1ad35664a5b3e165f0dc882196c2
DESARROLLO\Luis Cortez <tonyayalagomez@hotmail.com>
2019-03-26 12:37:43 -0600

    cambios gestor

--- js/gestor.js
+++ js/gestor.js
@@ -22,3 +22,34 @@ $("#reduce").click( function () {
 	$(".phone_box").removeClass("phone_box_reduce");
 	$("textarea.materialize-textarea").css("height", "20vh");
 });
+
+//Intro Modals
+$("#m_gestiones").on("click", () => {
+	$('#modal_gestiones').modal('open');
+});
+$("#m_bancarias").on("click", () => {
+	$('#modal_bancarias').modal('open');
+});
+$("#m_pagos").on("click", () => {
+	$('#modal_pagos').modal('open');
+});
+$("#m_agenda").on("click", () => {
+	$('#modal_agenda').modal('open');
+});
+$("#m_visitas").on("click", () => {
+	$('#modal_visitas').modal('open');
+});
+$("#m_convenios").on("click", () => {
+	$('#modal_convenios').modal('open');
+});
+$("#m_config").on("click", () => {
+	$('#modal_config').modal('open');
+});
+$("#m_diarios").on("click", () => {
+	$('#modal_diarios').modal('open');
+});
+$("#m_saldos").on("click", () => {
+	$('#modal_saldos').modal('open');
+});
+
+// End Modals
