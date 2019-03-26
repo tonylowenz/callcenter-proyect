5c8d4ccfb76b5c700398ae195d0657f72792b5f9
tony lowenz <>
2019-03-26 15:48:09 -0600

    imagnes de bancos y modificacion en agenda y ref bancarias

--- js/gestor.js
+++ js/gestor.js
@@ -4,6 +4,27 @@ $(document).on('ready', function(){
 	$('input#input_text, textarea#tgestion').characterCounter();
 	$('.tooltipped').tooltip({delay: 50});
 	$('.tooltipped').tooltip({position:"left"});
+	$('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15, // Creates a dropdown of 15 years to control year,
+	    today: 'Today',
+	    clear: 'Clear',
+	    close: 'Ok',
+	    closeOnSelect: false // Close upon selecting a date,
+	    container: undefined, // ex. 'body' will append picker to body
+	});
+	$('.timepicker').pickatime({
+	    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+	    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+	    twelvehour: false, // Use AM/PM or 24-hour format
+	    donetext: 'OK', // text for done-button
+	    cleartext: 'Clear', // text for clear-button
+	    canceltext: 'Cancel', // Text for cancel-button,
+	    container: undefined, // ex. 'body' will append picker to body
+	    autoclose: false, // automatic close timepicker
+	    ampmclickable: true, // make AM PM clickable
+	    aftershow: function(){} //Function for after opening timepicker
+	});
 
 
 	
@@ -27,27 +48,35 @@ $("#reduce").click( function () {
 $("#m_gestiones").on("click", () => {
 	$('#modal_gestiones').modal('open');
 });
+
 $("#m_bancarias").on("click", () => {
 	$('#modal_bancarias').modal('open');
 });
+
 $("#m_pagos").on("click", () => {
 	$('#modal_pagos').modal('open');
 });
+
 $("#m_agenda").on("click", () => {
 	$('#modal_agenda').modal('open');
 });
+
 $("#m_visitas").on("click", () => {
 	$('#modal_visitas').modal('open');
 });
+
 $("#m_convenios").on("click", () => {
 	$('#modal_convenios').modal('open');
 });
+
 $("#m_config").on("click", () => {
 	$('#modal_config').modal('open');
 });
+
 $("#m_diarios").on("click", () => {
 	$('#modal_diarios').modal('open');
 });
+
 $("#m_saldos").on("click", () => {
 	$('#modal_saldos').modal('open');
 });
