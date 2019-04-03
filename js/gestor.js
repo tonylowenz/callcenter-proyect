5ffd6b450e5b57780a05ff0977f52980e5da23f7
tony lowenz <>
2019-04-03 14:03:28 -0600

    cambios materialize

--- js/gestor.js
+++ js/gestor.js
@@ -1,14 +1,30 @@
 $(document).on('ready', function(){
-	$('select').material_select();
+	$('.fixed-action-btn').floatingActionButton();
+	$('select').formSelect();
 	$('.modal').modal();
 	$('input#input_text, textarea#tgestion').characterCounter();
 	$('.tooltipped').tooltip({delay: 50});
 	$('.tooltipped').tooltip({position:"left"});
-	$('.datepicker').pickadate({
-		container:"body"
+	$('.datepicker').datepicker({
+		container:"body",
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year,
+		today: 'Today',
+		clear: 'Clear',
+		close: 'Ok',
+		closeOnSelect: false // Close upon selecting a date,
 	});
-	$('.timepicker').pickatime({
-		container:"body"
+	$('.timepicker').timepicker({
+		default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+	    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+	    twelvehour: false, // Use AM/PM or 24-hour format
+	    donetext: 'OK', // text for done-button
+	    cleartext: 'Clear', // text for clear-button
+	    canceltext: 'Cancel', // Text for cancel-button,
+	    container: "body", // ex. 'body' will append picker to body
+	    autoclose: false, // automatic close timepicker
+	    ampmclickable: true, // make AM PM clickable
+	    aftershow: function(){} //Function for after opening timepicker
 	});
 });
 
